6c63972c30e7616ba34129b3eceaec9c2f4d40b0
Vviniciuss13 <viniciusfravolini@gmail.com>
2023-08-28 16:41:20 -0300

    update login

--- rota/login/index.php
+++ rota/login/index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    unset($_SESSION['user']);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,28 +9,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="assets/css/login2.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-    crossorigin="anonymous"
-    />
     <link rel="stylesheet" href="assets/css/fonts.css">
+    <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
-    <div class="content">
+    <div class="content container pt-5">
         <form action="<?= url("verificar") ?>" method="post">
             <div class="form-conteudo">
-                <label for="usuario"><i class="fas fa-user"></i>Nome de Usuario</label>
-                <input type="text" name="usuario" id="usuario">
-                <br>
-                <label for="senha"><i class="fas fa-lock"></i>Senha</label>
-                <input type="password" name="senha" id="senha">
-                <br>
+                <div class="mb-3 mt-3">
+                    <label for="usuario" class="form-label"><i class="fas fa-user form-label"></i>Nome de Usuario</label>
+                    <input type="text" name="usuario" id="usuario" class="form-control">
+                </div>
+                <div class="mb-3 mt-3">
+                    <label for="senha" class="form-label"><i class="fas fa-lock"></i>Senha</label>
+                    <input type="password" name="senha" id="senha" class="form-control">
+                </div>
                 <p style="float: left">Esqueceu sua senha? <a href="#.html" style="color: white; opacity: 0.8;">Clique Aqui</a></p>
-                <button type="submit" class="btn rounded-pill btn-cadastrar">Entrar</button>
+                <div>
+                    <button type="submit" class="btn rounded-pill btn-cadastrar">Entrar</button>
+                </div>
             </div>
         </form>
     </div>
--- source/config.php
+++ source/config.php
@@ -1,5 +1,5 @@
 <?php
-define("URL_BASE","http://projetospw/TCC%2022.0/");
+define("URL_BASE","http://localhost/projeto/Agendamento_Psicologa/");
 define("CSS","assets/css/");
 define("JS","assets/js/");
 define("IMG","assets/img/");
